009988a3e3b8ab996842ad4fde7e042bedeff040
Vinicius <jorgevinicius2002@hotmail.com>
2025-05-30 16:09:52 -0300

    config tailwindcss

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="min-h-screen p-4">
+    <p>
+      Mussum Ipsum, cacilds vidis litro abertis. Admodum accumsan disputationi
+      eu sit. Vide electram sadipscing et per. Per aumento de cachacis, eu
+      reclamis. Paisis, filhis, espiritis santis. Cevadis im ampola pa arma uma
+      pindureta.
+    </p>
+  </div>
+</template>
